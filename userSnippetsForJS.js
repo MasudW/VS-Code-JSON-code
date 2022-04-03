707276da7e26931c4f8941a7bd54a62e96b5b5c2
Md Majidul Islam Masud <85607318+MasudW@users.noreply.github.com>
2022-04-03 14:10:53 +0600

    v 1.1

---
+++ userSnippetsForJS.js
@@ -0,0 +1,12 @@
+{
+// jQuery Snippets goes here
+//
+	//JqfadeToggle Snippet:
+	"jqfadeToggle": {
+		"prefix": "jqfadeToggle",
+		"body": [
+			"$(${1:selector}).fadeToggle();"
+		],
+	},
+	// next
+}
